3128b5f55b91a58bbc9b67d75fba8e3f7917249a
Eugene Eeo <packwolf58@gmail.com>
2015-07-15 15:18:03 +0800

    add documentation

--- stateless.js
+++ stateless.js
@@ -8,26 +8,56 @@
     }
   };
 
+  /**
+   * Clears the array of registered handlers.
+   */
   var clear = function() {
     handlers.length = 0;
   };
 
+  /**
+   * Mark the state as processed- if the hash is
+   * changed to the identical state then no
+   * Stateless handlers will be fired.
+   *
+   * @param {String} state
+   */
   var skip = function(state) {
     previous[0] = '#' + state;
   };
 
+  /**
+   * Pull the current state from the URL, and
+   * forcefully runs the handlers regardless
+   * of whether the hash has changed.
+   */
   var pull = function() {
     var fragment = window.location.hash.substr(1);
     fireHandlers(fragment);
     skip(fragment);
   };
 
+  /**
+   * Set the current hash fragment as the given
+   * state. Note that the '#' character will be
+   * automatically added, thus it is not necessary
+   * to preceed your strings with '#'.
+   *
+   * @param {String} state
+   */
   var push = function(state) {
     window.location.hash = '#' + state;
   };
 
-  var onChange = function(f) {
-    handlers.push(f);
+  /**
+   * Register a Stateless handler. Handlers will
+   * be passed the hash fragment after the hash
+   * character.
+   *
+   * @param {Function} handler
+   */
+  var onChange = function(handler) {
+    handlers.push(handler);
   };
 
   window.addEventListener('hashchange', function(ev) {
