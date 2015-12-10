2fb549ecfe185d0d3786d287d3b743fafd3e57a2
Eeo Jun <packwolf58@gmail.com>
2015-12-10 19:19:38 +0800

    Rewrite for cleaner code

--- stateless.js
+++ stateless.js
@@ -11,8 +11,8 @@ var Stateless = (function(){
   var handlers = [];
   var hashRegex = /#(.*)$/;
 
-  var fireHandlers = function(state) {
-    for (var i = 0; i < handlers.length; i++) {
+  var fire = function(state) {
+    for (var i=0; i < handlers.length; i++) {
       handlers[i](state);
     }
   };
@@ -24,63 +24,10 @@ var Stateless = (function(){
     return hash ? hash[1] : '';
   };
 
-  /*
-   * Register a Stateless handler.
-   */
-  var onChange = function(handler) {
-    handlers.push(handler);
-  };
-
-  /*
-   * Removes the given handler from the registered
-   * handlers.
-   */
-  var off = function(handler) {
-    var idx = handlers.indexOf(handler);
+  var off = function(fn) {
+    var idx = handlers.indexOf(fn);
     if (idx !== -1)
       handlers.splice(idx, 1);
-  };
-
-  /*
-   * Clears the array of registered handlers.
-   */
-  var clear = function() {
-    handlers = [];
-  };
-
-  /*
-   * Mark the state as processed- if the hash is
-   * changed to the identical state, no Stateless
-   * handlers will be fired.
-   */
-  var skip = function(state) {
-    skipped[state] = true;
-  };
-
-  /* 
-   * Pull the current state from the URL.
-   * Forcefully runs the handlers regardless
-   * of whether the hash has changed.
-   */
-  var pull = function() {
-    fireHandlers(getState());
-  };
-
-  /*
-   * Set the current hash fragment as the given
-   * state.
-   */
-  var push = function(state) {
-    window.location.hash = '#' + state;
-  };
-
-  /*
-   * Replaces the current hash with the given state.
-   * Does not push another entry into the browser
-   * history.
-   */
-  var replace = function(state) {
-    window.location.replace('#' + state);
   };
 
   window.addEventListener('hashchange', function() {
@@ -88,16 +35,19 @@ var Stateless = (function(){
     if (skipped[state])
       delete skipped[state];
     else
-      fireHandlers(state);
+      fire(state);
   });
 
   return {
+    // Control
     off: off,
-    push: push,
-    pull: pull,
-    skip: skip,
-    replace: replace,
-    onChange: onChange,
-    clear: clear,
+    clear:    function() { handlers = []; },
+    skip:     function(s) { skipped[s] = true; },
+    onChange: function(fn) { handlers.push(fn); },
+
+    // Firing
+    pull:     function() { fire(getState()); },
+    push:     function(state) { window.location.hash = '#' + state; },
+    replace:  function(state) { window.location.replace('#' + state); },
   };
 })();
